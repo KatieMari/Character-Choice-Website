5045dedf22146d1e65eabe437ea1fec6a949be09
KatieMari <katie.glover@student.norwichuni.ac.uk>
2024-10-15 10:16:21 +0100

    added alt text and character name

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,23 @@
 console.log('hello world')
 
 // Defines Sources for each Charcter Choice
-const imageOne = "Assets/Images/Minion.png";
-const imageTwo = "Assets/Images/Gru.png";
-const imageThree = "Assets/Images/Dr Nefario.png";
+const image1 = "Assets/Images/Cat 1.avif";
+const image2 = "Assets/Images/Cat 2.jpg";
+const image3 = "Assets/Images/Cat 3.jpg";
 
 // Defines HTML Element Constants
 const  myButton = document.getElementById("my-button");
 const  myParagraph = document.getElementById("my-paragraph");
 const  characterImage = document.getElementById("character-img");
+
+const altText1 = ""
+const altText2 = ""
+const altText3 = ""
+
+// Defien Text for Paragraph for each Character Choice
+const characterName1 ="Charlie";
+const characterName2 ="Leo";
+const characterName3 ="Birdie";
 
 // Store which Character is Currently being Displayed
 let characterCounter = 1;
@@ -26,24 +35,30 @@ if (characterCounter > 3) {
 }
 
 // Update Paragraph Text with New Character
-myParagraph.innerHTML = "I am on Character" + characterCounter;
+myParagraph.innerHTML = "Charlie" + characterCounter;
 
 // Find Image Related to Specific Character Counter
 console.log("Testing if Character Counter is One")
 if (characterCounter === 1){
-    characterImage.src = imageOne;
+    characterImage.src = image1;
+    characterImage.alt = altText1;
+    myParagraph.innerHTML = characterName1;
     return;
 }
 
 console.log("Testing if Character Counter is One")
 if (characterCounter === 2){
-    characterImage.src = imageTwo;
+    characterImage.src = image2;
+    characterImage.alt = altText2;
+    myParagraph.innerHTML = characterName2;
     return;
 }
 
 console.log("Testing if Character Counter is One")
 if (characterCounter === 3){
-    characterImage.src = imageThree;
+    characterImage.src = image3;
+    characterImage.alt = altText3;
+    myParagraph.innerHTML = characterName3;
     return;
 }
 
