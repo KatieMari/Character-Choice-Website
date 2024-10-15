72f77c594ab9ca6bcddfa6bbfc30f6c1ee401fab
KatieMari <katie.glover@student.norwichuni.ac.uk>
2024-10-15 10:40:28 +0100

    condensed IF

--- JS/script.js
+++ JS/script.js
@@ -1,68 +1,42 @@
 console.log('hello world')
 
 // Defines Sources for each Charcter Choice
-const image1 = "Assets/Images/Cat 1.avif";
-const image2 = "Assets/Images/Cat 2.jpg";
-const image3 = "Assets/Images/Cat 3.jpg";
+const imageSources = ["Assets/Images/Cat 1.avif", "Assets/Images/Cat 2.jpg", "Assets/Images/Cat 3.jpg"];
+
+// Defines AltText for Images
+const altText = ["Grey Cat", "Ginger Cat", "Black & White Cat"]
+
+// // Define Text for Paragraph for each Character Choice
+const characterNames = ["Charlie", "Leo", "Birdie"];
 
 // Defines HTML Element Constants
-const  myButton = document.getElementById("my-button");
-const  myParagraph = document.getElementById("my-paragraph");
-const  characterImage = document.getElementById("character-img");
-
-const altText1 = ""
-const altText2 = ""
-const altText3 = ""
-
-// Defien Text for Paragraph for each Character Choice
-const characterName1 ="Charlie";
-const characterName2 ="Leo";
-const characterName3 ="Birdie";
+const myButton = document.getElementById("my-button");
+const myParagraph = document.getElementById("my-paragraph");
+const characterImage = document.getElementById("character-img");
 
 // Store which Character is Currently being Displayed
 let characterCounter = 1;
 
-function clickFunction(){
+function clickFunction() {
     console.log("===== NEW TESTING STARTED =====")
 
+    // Update Character Counter
+    characterCounter = characterCounter + 1;
 
-// Update Character Counter
-characterCounter = characterCounter + 1;
+    // If Beyond 3 -> Set Character Counter to 1
+    if (characterCounter > 3) {
+        characterCounter = 1;
+    }
 
-// If Beyond 3 -> Set Character Counter to 1
-if (characterCounter > 3) {
-    characterCounter = 1;
-}
+    // Update Paragraph Text with New Character
+    myParagraph.innerHTML = "Charlie" + characterCounter;
 
-// Update Paragraph Text with New Character
-myParagraph.innerHTML = "Charlie" + characterCounter;
+    console.log('character counter', characterCounter);
 
-// Find Image Related to Specific Character Counter
-console.log("Testing if Character Counter is One")
-if (characterCounter === 1){
-    characterImage.src = image1;
-    characterImage.alt = altText1;
-    myParagraph.innerHTML = characterName1;
-    return;
-}
-
-console.log("Testing if Character Counter is One")
-if (characterCounter === 2){
-    characterImage.src = image2;
-    characterImage.alt = altText2;
-    myParagraph.innerHTML = characterName2;
-    return;
-}
-
-console.log("Testing if Character Counter is One")
-if (characterCounter === 3){
-    characterImage.src = image3;
-    characterImage.alt = altText3;
-    myParagraph.innerHTML = characterName3;
-    return;
-}
-
-console.log("No More Testing To Do");
+    // Find Image Related to Specific Character Counter
+    characterImage.src = imageSources[characterCounter - 1];
+    characterImage.alt = altText[characterCounter - 1];
+    myParagraph.innerHTML = characterNames[characterCounter - 1];
 }
 
 myButton.onclick = clickFunction
